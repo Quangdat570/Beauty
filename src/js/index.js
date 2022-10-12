bfd3c483a3be5963601eb1092c2345bc25e32d10
Quang Dat <Quangdat570@gmail.com>
2022-10-12 20:59:32 +0700

    sua loi reponsive

--- src/js/index.js
+++ src/js/index.js
@@ -32,15 +32,20 @@ const cart = document.querySelectorAll('.cart');
 const name = document.querySelector('.titlel');
 
 const imgProduct = document.querySelector('.img-products');
+const prices = document.querySelector('.prices span');
 
 btn.forEach(function(a,index) {
     a.addEventListener('click',function(event){{
       var product = a.parentElement;
-      var ongnoi = product.parentElement;
+      var ongnoi = product.parentElement.parentElement;
       var productImg = ongnoi.querySelector('img').src;
       var productName = ongnoi.querySelector(".title-product").innerText;
+      var priceProductImg = ongnoi.querySelector('.price').innerText;
       imgProduct.src = productImg;
       name.textContent = productName;
+      prices.textContent = priceProductImg;
+      console.log(priceProductImg,index);
+      
       
     }})
 })
@@ -51,11 +56,11 @@ const bag = document.querySelectorAll('.btn-cart');
 bag.forEach(function(a,index) {
   a.addEventListener('click',function(event) {
     var item = a.parentElement;
-    var father = item.parentElement;
+    var father = item.parentElement.parentElement;
     var cartImg = father.querySelector('img').src;
     var cartName = father.querySelector('.title-product').innerText;
     var cartPrice = father.querySelector('.price').innerText;
-    
+    alert ("Đã thêm sản phẩm vào giỏ hàng");
     addcart(cartImg,cartName,cartPrice);
   })
 })
@@ -71,7 +76,7 @@ function addcart(cartImg,cartName,cartPrice) {
       return
     }
   }
-  var trContent = ` <tr><td class="body-plus"><img src="`+cartImg+`" alt="" ><span class="name-cart">`+cartName+`</span></td><td><span class="prices">`+cartPrice+`</span><span></span></td><td><input type="number" value="1" min="1" style="width: 30px;outline: none;"></td><td style="cursor: pointer;"><span class="delete-cart">Xóa</span></td></tr>`;
+  var trContent = ` <tr><td class="body-plus"><img src="`+cartImg+`" alt="" ><span class="name-cart">`+cartName+`</span></td><td><span class="prices">`+cartPrice+`</span><span></span></td><td><input type="number" value="1" min="1" style="width: 40px;outline: none;"></td><td style="cursor: pointer;"><span class="delete-cart">Xóa</span></td></tr>`;
   addtr.innerHTML = trContent;
   var cartTable = document.querySelector('tbody');
   cartTable.append(addtr);
