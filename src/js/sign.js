2789b234c53fbae97a5aa68ecfc96b6131f28d75
Quang Dat <Quangdat570@gmail.com>
2022-10-13 23:05:21 +0700

    them validate form

--- src/js/sign.js
+++ src/js/sign.js
@@ -4,5 +4,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+// import { formatPostcssSourceMap } from "vite";
 
 
+
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+
+function showError(input,message) {
+    let parent = input.parentElement;
+    let text = parent.querySelector('#text');
+    parent.classList.add('error');
+    text.innerText = message;
+    
+}
+
+function showSuccess(input) {
+    let parent = input.parentElement;
+    let text = parent.querySelector('#text');
+    parent.classList.remove('error');
+    text.innerText = '';
+   
+}
+
+function checkEmtyError(listInput) {
+    listInput.forEach(input => {
+        input.value = input.value.trim();
+        if (!input.value) {
+            showError(input, 'Không được để trống');
+        }
+        else {
+            showSuccess(input);
+        }
+    })
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    checkEmtyError([email,password]);
+})
