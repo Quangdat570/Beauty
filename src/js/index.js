00f07ff2a03bda285d10d90a39084a39185ebb5c
Quang Dat <Quangdat570@gmail.com>
2022-10-14 18:39:11 +0700

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-import "../css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "../css/index.css";
 
 // flash sale
 
